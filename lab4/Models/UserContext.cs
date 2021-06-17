2269bd4601c8424d52245ccdc9e156ff46623e77
notpouxie <73078800+notpouxie@users.noreply.github.com>
2021-06-17 21:58:33 +0700

    Add files via upload

---
+++ lab4/Models/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace lab4.Models
+{
+    public class UserContext : DbContext
+    {
+
+        public UserContext(DbContextOptions<UserContext> options)
+           : base(options)
+        {
+            Database.EnsureCreated();   
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
